7380aeccba35f51fe8f453b8eb6fd8c6a7a11d8d
mahiuddin-dev <codermahiuddin@gmail.com>
2023-09-15 16:55:05 +0600

    code quality with flake8

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from blog.models import Post
 
-# Register your models here.
+
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug')
     # populate_fields slug to title
--- blog/tests.py
+++ blog/tests.py
@@ -1,13 +1,13 @@
 from django.test import TestCase
 from blog.models import Post
 
-# Create your tests here.
 
+# Create your tests here.
 class PostTest(TestCase):
-    
+
     def setUp(self):
         self.post = Post.objects.create(title='Test Title', author='Test Author', slug='test-title')
-    
+
     def test_post_model(self):
         data = self.post
         self.assertTrue(isinstance(data, Post))

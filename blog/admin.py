40204fcf4e439b37bd35d0e197b8b3e29023dfbf
mahiuddin-dev <codermahiuddin@gmail.com>
2023-09-15 16:16:27 +0600

    update

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from blog.models import Post
+
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug')
+    # populate_fields slug to title
+    # prepopulated_fields = {'slug': ('title',)}
+    prepopulated_fields = {'slug': ['title', ]}
+
+
+admin.site.register(Post, PostAdmin)
